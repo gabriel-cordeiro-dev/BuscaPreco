6968d045117708d0917fad829cf7503fbe1e2eee
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2021-10-28 20:45:45 -0300

    .

--- server/models/mercado.js
+++ server/models/mercado.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize')
 const conn = require('../db')
 
-const Mercado = conn.define('mercados', {
+const Mercado = conn.define('mercado', {
     mercado_nome: DataTypes.STRING
 })
 
