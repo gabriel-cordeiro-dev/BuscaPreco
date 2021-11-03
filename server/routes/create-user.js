1d2bbc6433d7b2a3445b7173fae992418e9f228d
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2021-11-03 14:36:07 -0300

    Arrumando scripts de login
    Tentando arrumar script de login.
    que por algum motivo da sempre o erro de senha errada.
    
    Não arrumado ainda.

--- server/routes/create-user.js
+++ server/routes/create-user.js
@@ -1,28 +1,64 @@
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-const router = require('express').Router()
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const { check, body, validationResult } = require('express-validator');
 
-const salt = 10;
+require('dotenv').config();
 
-router.post("/criar", (req, res) => {
-  bcrypt.hash(req.body.password, salt, (err, hash) => {
-    if (err) {
-      console.log("Erro ao cifrar senha", err);
-    } else {
-      console.log("hash", hash);
+router.post('/criar', [
+    check('username', "Login é um campo obrigatório")
+        .trim().escape().notEmpty(),
+    check('password', "Senha é um campo obrigatório")
+        .trim().escape().notEmpty()
+], async (req, res) => {
+    try {
+        const erros = validationResult(req);
 
-      User.create({
-        username: req.body.username,
-        password: hash,
-      })
-        .then((user) => {
-          return res.json({ msg: "Usuário cadastrado com sucesso" });
-        })
-        .catch((err) => {
-          return res.status(500).json({ msg: err });
+        const contextoErros = {
+            erros: erros.array(),
+        };
+
+        console.log(erros);
+
+        if (!erros.isEmpty() || contextoErros.erros.length > 0) {
+            return res.status(422).json(contextoErros);
+        } else {
+            const user = await User.findOne({
+                where: { username: req.body.username }
+            });
+
+            if (user) {
+                console.log('Já existe usuário com esse login');
+                return res.status(400).json({
+                    err: 'Já existe usuário com esse login'
+                })
+            }
+
+            bcrypt.hash(req.body.password, 10, async (err, hash) => {
+                if (err) {
+                    console.log('Erro ao gerar hash para a senha', err);
+                } else {
+                    console.log('hash: ', hash);
+
+                    const newUser = await User.create({
+                        username: req.body.username,
+                        password: hash,
+                    });
+
+                    return res.json({
+                        msg: 'Usuário criado com sucesso!',
+                        user: newUser.username
+                    })
+                }
+            })
+
+        }
+    } catch (error) {
+        console.log('ERROR:', error);
+        return res.status(500).json({
+            err: error
         });
     }
-  });
 });
 
-module.exports = router
+module.exports = router;
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,37 +1,65 @@
-const router = require('express').Router()
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const User = require('../models/user')
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const { check, body, validationResult } = require('express-validator');
 
-require('../db')
+require('dotenv').config();
 
-router.post('/login', (req, res) => {
-    User.findOne({
-        where: { username: req.body.username }
-    })
-    .then((user) => {
-        if (user) {
-            bcrypt.compare(req.body.password, user.password).then( result => {
-                if (result) {
-                    const token = jwt.sign({
-                        username: user.username
-                    }, process.env.SECRET)
+router.post('/login', [
+    check('username', "Login é um campo obrigatório")
+        .trim().escape().notEmpty(),
+    check('password', "Senha é um campo obrigatório")
+        .trim().escape().notEmpty()
+], async (req, res) => {
+    try {
+        const erros = validationResult(req);
 
-                    return res.json({token: token})
-                } else{
-                    console.log("Senhas não conferem")
-                    return res.status(400).json({ err: "Usuário e senha incorretos"})
-                }
-            })
+        const contextoErros = {
+            erros: erros.array(),
+        };
+
+        console.log(erros);
+
+        if (!erros.isEmpty() || contextoErros.erros.length > 0) {
+            return res.status(422).json(contextoErros);
         } else {
-            console.log("Não encontrou usuário com login")
-            return res.status(404).json({ err: "Usuário e senha incorretos"})
-        }
-    }).catch( err => {
-        return res.status(500).json({
-            err: err.message
-        })
-    })
-})
+            const user = await User.findOne({
+                where: { username: req.body.username }
+            });
 
-module.exports = router
+            if (!user) {
+                console.log("Não encontrou usuário com o login");
+                return res.status(400).json({
+                    err: 'Usuário/senha incorretos'
+                })
+            }
+
+            const result = await bcrypt.compare(req.body.password, user.password);
+
+            if (result) {
+                const token = jwt.sign({
+                    id: user.id,
+                    username: user.username
+                }, process.env.SECRET);
+
+                console.log("token: ", token)
+                return res.json({
+                    token: token
+                });
+            } else {
+                console.log("Não encontrou usuário com a senha");
+                return res.status(400).json({
+                    err: 'Usuário/senha incorretos'
+                })
+            }
+        }
+    } catch (error) {
+        console.log('ERROR:', error);
+        return res.status(500).json({
+            err: error
+        });
+    }
+});
+
+module.exports = router;
