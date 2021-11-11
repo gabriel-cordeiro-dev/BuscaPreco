53faf7a1be04ac353e0cd7d9e463dc0e43bf5b95
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2021-11-10 21:54:35 -0300

    .

--- frontend/src/containers/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import Routes from '../components/Routes'
-
-const App = () => <Routes />;
-
-export default App;
